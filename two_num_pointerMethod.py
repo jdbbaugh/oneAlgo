7ea385378ff3d1e37a083b0b34e91787c17bd285
jdbbaugh <jbaugh78@msn.com>
2019-05-29 09:51:36 -0500

    clean up

--- two_num_pointerMethod.py
+++ two_num_pointerMethod.py
@@ -20,31 +20,6 @@ def dosNumSum(array, target_sum):
       pointer_left = pointer_left + 1
   return answer_array
 
-
-# def two_number_sum(array_nums, target):
-#   # print(len(array_nums) -1, target)
-
-#   array_count = len(array_nums)
-#   i = 0
-
-#   finalArray = []
-#   while i < array_count:
-#     # print(i)
-#     for num in array_nums:
-#       if num != array_nums[i]:
-#         test_for_target = num + array_nums[i]
-#         if test_for_target == target:
-#           print(f'{num} and {array_nums[i]} equal target number {target} ')
-#           newArray = [num,array_nums[i]]
-#           newArray.sort()
-#           finalArray.append(newArray)
-#     i = i + 1
-
-#   return finalArray
-
-# for item in two_number_sum(array,target_sum):
-#   print(item)
-
 print(dosNumSum(array, target_sum))
 
 
